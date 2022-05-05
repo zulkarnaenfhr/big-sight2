7a29178fedc316637eb29ce0db02ba67c310097e
zulkarnaenfhr <fahrizulkarnaen1@gmail.com>
2022-05-05 15:44:31 +0700

    Big Sight Landing page Section 3 Done Responsive

--- src/container/BigSight-LandingPage/BigSight-LandingPage-Section3/BigSight-LandingPage-Section3.jsx
+++ src/container/BigSight-LandingPage/BigSight-LandingPage-Section3/BigSight-LandingPage-Section3.jsx
@@ -1,11 +1,42 @@
 import React, { Component } from "react";
+import { Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import "./BigSight-LandingPage-Section3.css";
 
 class BigsightLandingpageSection3 extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            width: "",
+        };
+    }
+    componentDidMount() {
+        this.setState({
+            width: window.innerWidth,
+        });
+    }
     render() {
         return (
             <div id="BigSightLandingPage-Section3">
-                <h1>masuk</h1>
+                <Container>
+                    <h1 data-aos="fade-up" data-aos-offset="0" className="BigSight-LandingPage-Section3-Title">
+                        LET'S TRY OUR DEMO APPS
+                    </h1>
+                    <p data-aos="fade-up" data-aos-offset="0" className="BigSight-LandingPage-Section3-Desc">
+                        Discover how our products are implemented in real world situations and {this.state.width > 650 ? <br /> : ""} used in various use case through our interactive demo app.
+                    </p>
+                    <div data-aos="fade-up" data-aos-offset="0">
+                        <Link
+                            className="BigSight-LandingPage-Section3-Button-Container"
+                            to={{
+                                pathname: `notset`,
+                            }}
+                        >
+                            <button className="BigSight-LandingPage-Section3-Button">FREE TRIAL</button>
+                        </Link>
+                    </div>{" "}
+                </Container>
             </div>
         );
     }
